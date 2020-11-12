75c7b624e562fe0a8a4bf147666aa36ae535aa70
jijojij <grinenkoivan@gmail.com>
2020-11-12 20:50:50 +0300

    компоненты

--- star-wars/src/index.js
+++ star-wars/src/index.js
@@ -1,33 +1,6 @@
-class SwapService {
-    _apiBase = 'https://swapi.dev/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-    async getResource(url){
-        const res = await fetch(`${this._apiBase}/` + url);
-        if (!res.ok)
-            throw new Error(`Could not fetch ${url}`)
-        const body = await res.json();
-        return body;
-    }
-
-    async getAllPeople(){
-        const res = await this.getResource(`people`);
-        return res.results;
-    }
-
-    getPerson(id){
-        return this.getResource(`people/${id}`);
-    }
-
-    getPlanet(id){
-        return this.getResource(`planets/${id}`);
-    }
-
-    async getAllStarShips(){
-        const res = await this.getResource(`starships`);
-        return res.results;
-    }
-
-    getStartShip(id){
-        return this.getResource(`starships/${id}`);
-    }
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
