549f6159fdfcd27106e9db08c7821208ce1b424e
jijojij <grinenkoivan@gmail.com>
2020-11-12 20:16:29 +0300

    накидал api-client

--- star-wars/src/index.js
+++ star-wars/src/index.js
@@ -1,13 +1,33 @@
-const getResource = async (url) => {
-    const res = await fetch(url);
-    if (!res.ok){
-        throw new Error(`Could not fetch ${url}`)
+class SwapService {
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url){
+        const res = await fetch(`${this._apiBase}/` + url);
+        if (!res.ok)
+            throw new Error(`Could not fetch ${url}`)
+        const body = await res.json();
+        return body;
     }
-    const body = await res.json();
-    return body;
+
+    async getAllPeople(){
+        const res = await this.getResource(`people`);
+        return res.results;
+    }
+
+    getPerson(id){
+        return this.getResource(`people/${id}`);
+    }
+
+    getPlanet(id){
+        return this.getResource(`planets/${id}`);
+    }
+
+    async getAllStarShips(){
+        const res = await this.getResource(`starships`);
+        return res.results;
+    }
+
+    getStartShip(id){
+        return this.getResource(`starships/${id}`);
+    }
 }
-
-getResource('https://swapi.dev/api/people/1')
-    .then(x => console.log(x))
-    .catch(e=>console.log(e))
-
