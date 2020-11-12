369d6e6cd07a5718414d3e3afce304f1be033d13
jijojij <grinenkoivan@gmail.com>
2020-11-12 16:27:34 +0300

    fetch

--- star-wars/src/index.js
+++ star-wars/src/index.js
@@ -1,1 +1,13 @@
-console.log('qwe');
+const getResource = async (url) => {
+    const res = await fetch(url);
+    if (!res.ok){
+        throw new Error(`Could not fetch ${url}`)
+    }
+    const body = await res.json();
+    return body;
+}
+
+getResource('https://swapi.dev/api/people/1')
+    .then(x => console.log(x))
+    .catch(e=>console.log(e))
+
